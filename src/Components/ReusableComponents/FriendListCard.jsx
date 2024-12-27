a12d0f9b909056f8b80b8552bc3b62607c5bc890
Vignesh1066 <vignesh4974@gmail.com>
2024-12-28 04:52:45 +0530

    added friendlistcard and modify

--- src/Components/ReusableComponents/FriendListCard.jsx
+++ src/Components/ReusableComponents/FriendListCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import image from "../../assets/LoginImagebg.jpg";
 
 const FriendListCard = ({ friends }) => {
-    const backendURL = "http://localhost:7000";
+  const backendURL = "http://localhost:7000";
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
       {friends.map((friend, index) => (
@@ -10,16 +10,16 @@ const FriendListCard = ({ friends }) => {
           key={index}
           className="relative bg-white rounded-lg shadow-lg bg-gradient-to-t from-gray-800 via-gray-700 to-gray-700 overflow-hidden border group hover:shadow-2xl transition-shadow duration-300"
         >
-          <div className="absolute inset-0 bg-gradient-to-b from-orange-400 via-orange-500 to-orange-600 opacity-0  group-hover:opacity-100 transition-opacity duration-500"></div>
-          
+          <div className="absolute inset-0 bg-gradient-to-b from-orange-400 via-orange-500 to-orange-600 opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
+
           <div className="relative z-10 p-4">
             <div className="flex items-center">
               <img
-               src={
-                friends.fileName
-                  ? `${backendURL}/upload/${friends.fileName}`
-                  : image
-              }
+                src={
+                  friend.fileName
+                    ? `${backendURL}/upload/${friend.fileName}`
+                    : image
+                }
                 alt={friend.userName}
                 className="w-16 h-16 rounded-full object-cover border-2 border-gray-300"
               />
@@ -34,11 +34,17 @@ const FriendListCard = ({ friends }) => {
               <h4 className="text-md font-semibold text-gray-700 mb-2">
                 Recently Watched Movies:
               </h4>
-              <ul className="list-disc list-inside text-gray-600 text-sm space-y-1">
-                {friend.watchedMovies.map((movie, idx) => (
-                  <li key={idx}>{movie}</li>
-                ))}
-              </ul>
+              {friend.watchedMovies.length > 0 ? (
+                <ul className="list-disc list-inside text-gray-600 text-sm space-y-1">
+                  {friend.watchedMovies.map((movie, idx) => (
+                    <li key={idx}>{movie}</li>
+                  ))}
+                </ul>
+              ) : (
+                <p className="text-sm text-gray-500">
+                  No movies watched recently.
+                </p>
+              )}
             </div>
           </div>
         </div>
